4cb356420c763fb6c74ee872d1530bf0adcbdccf
thatcasualdragon <thatcasualdragon@gmail.com>
2026-09-05 10:34:06 -0700

    added fps restriction

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
-
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 
