b9de655880506c32619fb676c00b105545b2158f
thatcasualdragon <thatcasualdragon@gmail.com>
2026-09-05 12:09:37 -0700

    update constants.py, player.py to add up/down movement

--- player.py
+++ player.py
@@ -16,15 +16,28 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    # draw the player's piece on the screen
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH)
 
     def rotate(self, dt):
         self.rotation = (constants.PLAYER_TURN_SPEED * dt) + self.rotation
 
+    # move the player's piece up, down, left, and right on screen
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_j]:
                 self.rotate(0 - dt)
         if keys[pygame.K_l]:
                 self.rotate(dt)
+        if keys[pygame.K_i]:
+                self.move(dt)
+        if keys[pygame.K_k]:
+                self.move(0 - dt)
+
+    #math for moving up and down on screen
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * constants.PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
