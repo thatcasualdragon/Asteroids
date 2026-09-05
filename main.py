9f14faec1bdfd4c87f325e0ea3d1e4758676f339
thatcasualdragon <thatcasualdragon@gmail.com>
2026-09-05 10:10:30 -0700

    update main.py after moving Asteroids folder

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while True:
-    log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
     screen.fill("black")
     pygame.display.flip()
 
